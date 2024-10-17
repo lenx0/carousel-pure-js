aca7e74e66d42aa491ad901286252bc5064284c2
thiago <thiagofreitas1106@gmail.com>
2024-10-17 16:28:57 -0300

    save

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-const slides = document.querySelectorAll('.slide');
+const slides = Array.from(document.querySelectorAll('.slide'));
+let currentIndex = 0;
+const slideInterval = 300000;
 
-slides[0].querySelector('.number').classList.add('active-number');
+function activateSlide(index) {
+    slides.forEach((s, i) => {
+        s.classList.remove('active');
+        s.querySelector('.number').classList.remove('active-number');
+    });
+    slides[index].classList.add('active');
+    slides[index].querySelector('.number').classList.add('active-number');
+}
 
-slides.forEach((slide) => {
+function moveSlideToEnd() {
+    const firstSlide = slides.shift();
+    slides.push(firstSlide);
+    const sliderContainer = document.querySelector('.slider');
+    sliderContainer.appendChild(firstSlide);
+}
+
+function moveClickedSlideToFront(clickedIndex) {
+    const clickedSlide = slides[clickedIndex];
+    slides.splice(clickedIndex, 1);
+    slides.unshift(clickedSlide);
+    const sliderContainer = document.querySelector('.slider');
+    sliderContainer.insertBefore(clickedSlide, sliderContainer.firstChild);
+}
+
+function nextSlide() {
+    moveSlideToEnd();
+    activateSlide(0);
+}
+
+let autoSlide = setInterval(() => {
+    nextSlide();
+}, slideInterval);
+
+slides.forEach((slide, index) => {
     slide.addEventListener('click', () => {
-        slides.forEach(s => {
-            s.classList.remove('active');
-            const number = s.querySelector('.number');
-            number.classList.remove('active-number');
-        });
-        
-        slide.classList.add('active');
-        const activeNumber = slide.querySelector('.number');
-        activeNumber.classList.add('active-number');
+        clearInterval(autoSlide);
+
+        if (!slide.classList.contains('active')) {
+            moveClickedSlideToFront(index);
+            activateSlide(0);
+        }
+
+        autoSlide = setInterval(() => {
+            nextSlide();
+        }, slideInterval);
     });
 });
 
-
+activateSlide(currentIndex);
