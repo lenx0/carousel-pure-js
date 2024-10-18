2ac05b4fa37cd4454af2bc7076804e60f74b6824
thiago <thiagofreitas1106@gmail.com>
2024-10-18 07:42:28 -0300

    fix slide click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const slides = Array.from(document.querySelectorAll('.slide'));
 let currentIndex = 0;
-const slideInterval = 300000;
+const slideInterval = 5000;
 
 function activateSlide(index) {
     slides.forEach((s, i) => {
@@ -11,42 +11,20 @@ function activateSlide(index) {
     slides[index].querySelector('.number').classList.add('active-number');
 }
 
-function moveSlideToEnd() {
-    const firstSlide = slides.shift();
-    slides.push(firstSlide);
-    const sliderContainer = document.querySelector('.slider');
-    sliderContainer.appendChild(firstSlide);
-}
-
-function moveClickedSlideToFront(clickedIndex) {
-    const clickedSlide = slides[clickedIndex];
-    slides.splice(clickedIndex, 1);
-    slides.unshift(clickedSlide);
-    const sliderContainer = document.querySelector('.slider');
-    sliderContainer.insertBefore(clickedSlide, sliderContainer.firstChild);
-}
-
 function nextSlide() {
-    moveSlideToEnd();
-    activateSlide(0);
+    currentIndex = (currentIndex + 1) % slides.length;
+    activateSlide(currentIndex);
 }
 
-let autoSlide = setInterval(() => {
-    nextSlide();
-}, slideInterval);
+let autoSlide = setInterval(nextSlide, slideInterval);
 
 slides.forEach((slide, index) => {
     slide.addEventListener('click', () => {
         clearInterval(autoSlide);
+        currentIndex = index;
+        activateSlide(currentIndex);
 
-        if (!slide.classList.contains('active')) {
-            moveClickedSlideToFront(index);
-            activateSlide(0);
-        }
-
-        autoSlide = setInterval(() => {
-            nextSlide();
-        }, slideInterval);
+        autoSlide = setInterval(nextSlide, slideInterval);
     });
 });
 
